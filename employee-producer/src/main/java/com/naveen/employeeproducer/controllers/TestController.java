adc2d329fa9179121cce201c9016441aba5b5a01
Naveen <naveen.chaurasia861@gmail.com>
2021-01-17 21:50:38 +0530

    first commit

---
+++ employee-producer/src/main/java/com/naveen/employeeproducer/controllers/TestController.java
@@ -0,0 +1,25 @@
+package com.naveen.employeeproducer.controllers;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.naveen.employeeproducer.models.Employee;
+//@EnableEurekaClient
+@RestController
+public class TestController {
+
+	@RequestMapping(value = "/employee", method = RequestMethod.GET)
+	public Employee firstPage() {
+
+		Employee emp = new Employee();
+		emp.setName("emp1");
+		emp.setDesignation("manager");
+		emp.setEmpId("1");
+		emp.setSalary(3000);
+
+		return emp;
+	}
+
+}
